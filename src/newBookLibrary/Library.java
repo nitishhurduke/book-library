7481cea19973e63196dba376dcc9d3c353784ede
nitishhurduke <nitishhurduke@gmail.com>
2021-09-17 08:42:08 +0530

    Resolved Instance problem
    created single Main Library instance and referred it to all methods

--- src/newBookLibrary/Library.java
+++ src/newBookLibrary/Library.java
@@ -8,42 +8,89 @@ import java.text.DateFormat;
 
 public class Library {
 	
+	
 	ArrayList<Book> books = new ArrayList<Book>();
 	List<String> bookList = new ArrayList<String>();
 	ArrayList<Members> members = new ArrayList<Members>();
-	List<String>membersList = new ArrayList<String>();
+	List<String> membersList = new ArrayList<String>();
 	
-	public void initializeLibrary()
-	{ 
+
+	public void initializeLibrary(Library l) {
 		System.out.println("How many books do you want to add initialy");
-		Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		int total = sc.nextInt();
-		addBook(total);
+		addBook(total,l);		
 	}
 
-
-	public void addBook(int total) {
+	public void addBook(int total,Library l) {
 		System.out.println("Add Books");
 		Scanner bin =new Scanner(System.in);
+		
 		for(int i = 0 ; i< total; i++ )
-		{
+		{	
 			Book b = new Book();
-			
 			System.out.print((i+1)+". ");
 			b.bname = bin.nextLine(); 
-			 bookList.add(b.bname);
-			 books.add(b);	
+			books.add(b);
+			bookList.add(b.bname);
+			 		 	
 		}
+		 System.out.println("Books added Successfully...");
+		 System.out.println("Enter '1' to Add another book or '0'to Main Menu");
+		 int input = bin.nextInt();
+		 switch(input)
+		 {
+		 case 1: 
+			 	addBook(l);
+			 	break;
+		 case 0:
+			 
+			 	l.mainMenu(l);
+			 	break;
+		 }
 	}
 
+	public void addBook(Library l) {
 
-	public void addMembers()
-	{	
+		Scanner sc = new Scanner(System.in);
+		Scanner bin = new Scanner(System.in);
+		
+		System.out.println("Add Books");
+		System.out.println("How Many Books do you want to add : ");
+		int total = sc.nextInt();
+	
+		for (int i = 0; i < total; i++) 
+		{
+			Book b = new Book();
+			System.out.print((i + 1) + ". ");
+			b.bname = bin.nextLine();
+			books.add(b);
+			bookList.add(b.bname);
+					
+		}
+		 System.out.println("Books added Successfully...");
+		 System.out.println("Enter '1' to Add another book or '0'to Main Menu");
+		 int input = bin.nextInt();
+		 switch(input)
+		 {
+		 case 1: 
+			 	addBook(l);
+			 	break;
+		 case 0:
+			 	
+			 	l.mainMenu(l);
+			 	break;
+		 }
+		
+		
+	}
+
+	public void addMembers(Library l) {
 		Members member = new Members(); // Object member of Members class
-		
-		Scanner sc =new Scanner(System.in);
-		Scanner ad =new Scanner(System.in);
-		
+
+		Scanner sc = new Scanner(System.in);
+		Scanner ad = new Scanner(System.in);
+
 		System.out.println("  *New Member Registration Form* ");
 		member.mId += 1;
 		System.out.print("Enter Name :");
@@ -51,113 +98,176 @@ public class Library {
 		System.out.print("Enter Mobile number: ");
 		member.mob = sc.nextLong();
 		System.out.print("Enter Email Address : ");
-		member.address = ad.nextLine();	
+		member.address = ad.nextLine();
 		System.out.print("Enter Aadhaar Number : ");
 		member.adhar = sc.nextLong();
 
-		
+		members.add(member); // Adding member object to members ArrayList
 		membersList.add(member.mname);
-		members.add(member); //Adding member object to members ArrayList
 		
+
 		System.out.println("Member Successfully Added...");
-		
+		System.out.println(); 
+		System.out.println("Enter '1' to Add another member or '0'to Main Menu");
+		 int input = sc.nextInt();
+		 switch(input)
+		 {
+		 case 1: 
+			 	addMembers(l);
+			 	break;
+		 case 0:
+			 
+			 	l.mainMenu(l);
+			 	break;
+		 }
+
 	}
 
-
-	public void showMembers() 
-	{
+	public void showMembers(Library l) {
 		System.out.println("Id  |  Name  |  mobile no  |  Addhaar no  |  Address ");
 		for (Members m : members) {
-			System.out.println(m.id+" | "+m.mname+" | "+m.mob+" | "+m.adhar+" | "+m.address);
-		
-		}	
-	
-	}
-	public void bookShelf() {
-		int i = 1;
-		for(Book b : books) 
-		{	
-			if(b.issuer != null) { //Add Condition to fetch issue history
-			System.out.println(i+". "+b.bname+" "+b.issuer+" "+b.issueHistory.get(b.issueHistory.size() -1));
+			System.out.println(m.id + " | " + m.mname + " | " + m.mob + " | " + m.adhar + " | " + m.address);
 			
-			}else{
-				System.out.println(i+". "+b.bname);
-			}
-			 i++;
 		}
+		
+		l.subMenu(l);
+
 	}
 
-//	public void issueBook() 
-//	{
-//		System.out.println("Select Issuer from Library Members");
-//	}
-	public void issueBook() 
-	{	
-			
-			System.out.print("Please enter book name :");
-			Scanner sc = new Scanner(System.in);
-			String bname = sc.nextLine();
-			int index = bookList.indexOf(bname);
-			if(index == -1)
-			{
-				System.out.println("Invalid Book");
-				issueBook();
-			}
-			else 
-			{
-				Book b = books.get(index);
-				
-				System.out.print("Enter Issuers Name :");
-				Scanner sc1 = new Scanner(System.in);
-				String issuer = sc1.nextLine();
-				
-				int index1  = membersList.indexOf(issuer);
-				if(index1 == -1)
-				{
-					System.out.println("Invalid Issuer");
-					issueBook();
-				}
-				
-				else if(b.issueHistory.size() == b.returnHistory.size())
-				{
-					Members m = members.get(index1);
-					
-					b.issuer = m.mname;
-					
-					System.out.println("Enter date :");
-					Date currentDate = new Date();
-					String date = DateFormat.getInstance().format(currentDate);
-					b.issueHistory(date);
-					
-					System.out.println("book "+b.bname+" Successfully issued to "+b.issuer);
-										
-				}
-				else 	
-				{	
-					System.out.println("Book not Available");
-					issueBook();
-				}
-				
-			}							
-	}
-	public void returnBook()
-	{
+	public void bookShelf(Library l) {
 		
+		int i = 1;
+		for (Book b : books) {
+			System.out.println(i + ". " +b.bname);
+			i++;
+		}
+		l.subMenu(l);
 	}
-		
-	public static void main(String args[])
-	{
-		Library lib = new Library();
-		lib.initializeLibrary();
-		lib.addMembers();
-//		lib.addMembers();
-		lib.showMembers();
-		lib.bookShelf();
-		lib.issueBook();
-		lib.bookShelf();		
-		
-	}
-
-//		lib.showMembers();
 	
+	public void issueBook(Library l) {
+
+		System.out.print("Please enter book name :");
+		Scanner sc = new Scanner(System.in);
+		String bname = sc.nextLine();
+		int index = bookList.indexOf(bname);
+		if (index == -1) {
+			System.out.println("Invalid Book");
+			issueBook(l);
+		} else {
+			Book b = books.get(index);
+
+			System.out.print("Enter Issuers Name :");
+			Scanner sc1 = new Scanner(System.in);
+			String issuer = sc1.nextLine();
+
+			int index1 = membersList.indexOf(issuer);
+			if (index1 == -1) {
+				System.out.println("Invalid Issuer");
+				issueBook(l);
+			}
+
+			else if (b.issueHistory.size() == b.returnHistory.size()) {
+				Members m = members.get(index1);
+
+				b.issuer = m.mname;
+
+				System.out.println("Enter date :");
+				Date currentDate = new Date();
+				String date = DateFormat.getInstance().format(currentDate);
+				b.issueHistory(date);
+
+				System.out.println("book " + b.bname + " Successfully issued to " + b.issuer);
+
+			} else {
+				System.out.println("Book not Available");
+				issueBook(l);
+			}
+
+		}
+		
+		l.subMenu(l);
+	}
+
+	public void returnBook() {
+
+	}
+
+	public void mainMenu(Library l) {
+		System.out.println("Choose following operation : ");
+		System.out.println("1.Show BookShelf of Library");
+		System.out.println("2.Show All Registerd Members");
+		System.out.println("3.Issue Book");
+		System.out.println("4.Return Book");
+		System.out.println("5.Add New Books");
+		System.out.println("6.Add New Members");
+		System.out.println("7.Delete Books");
+		System.out.println("8.Delete Members");
+		System.out.println("9.Exit");
+
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+
+	
+
+		switch (input) {
+
+		case 1:
+			l.bookShelf(l);
+			break;
+		case 2:
+			l.showMembers(l);
+			break;
+		case 3:
+			l.issueBook(l);
+			break;
+		case 4:
+			l.returnBook();
+			break;
+		case 5:
+			l.addBook(l);
+			break;
+		case 6:
+			l.addMembers(l);
+			break;
+		case 7:
+			l.deleteBook();
+			break;
+		case 8: 
+			l.deleteMember();
+			break;
+		case 9:
+			System.exit(0);
+			break;					
+		}
+		
+	}
+	public void deleteBook()
+	{
+		
+		
+	}
+	public void deleteMember()
+	{
+		
+	}
+	public void subMenu(Library l)
+	{
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter '1' for 'Main Menu'Or '0' for 'Exit' ");
+		int input = sc.nextInt();
+		
+		switch(input)
+		{
+			case 1:l.mainMenu(l);
+					break;
+			case 0:System.exit(0);
+					break;		
+		}
+}
+
+	public static void main(String args[]) {
+		Library l = new Library();
+		l.initializeLibrary(l);
+		l.mainMenu(l);
+	}
 }
